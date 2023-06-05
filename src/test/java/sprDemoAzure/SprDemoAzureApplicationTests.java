b19ef32f2a1475debbb8b169524083dc174f0b6f
Shakul Hameed <shakulmsd07@gmail.com>
2023-06-05 18:09:27 +0530

    first commit

---
+++ src/test/java/sprDemoAzure/SprDemoAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package sprDemoAzure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprDemoAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
